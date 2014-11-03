39ca4e7ec6b17fc40da7d864bdf5befa3eb21808
lennonj <lennonj@indiana.edu>
2014-11-03 12:42:35 -0500

    small modifications to R assignment

--- Week1/Week1.R
+++ Week1/Week1.R
@@ -1,8 +1,8 @@
 ### INTRODUCTION TO WORKING IN R ###
 
 # SETTING YOUR WORKING DIRECTORY
-getwd() #erases previous working directory
-setwd("/Users/lennonj/GitHub/Quantitative_Biodiversity/Assignments/Week1")
+getwd() # prints current working directory
+setwd("/Users/lennonj/GitHub/Quantitative_Biodiversity/Assignments/Week1") # set workding dir
 
 # USING R AS A CALCULATOR #
 1+3 # addition
@@ -24,7 +24,7 @@ log(100) # natural log
   # importance of sequence of definintions
   # in R studio, see Environment tab
 
-a<-10
+a <- 10
 
 b <- a + 20
 
@@ -37,15 +37,15 @@ a <- 20 # now, what is b?
 
 # WORKING WITH SCALARS, VECTORS, AND MATRICES #
 
-w<-5 # x is a scalar
+w <- 5 # x is a scalar
 
-x<-c(2,3,6,w,w+7, 12,14) # x is a vector
+x <- c(2,3,6,w,w+7, 12,14) # x is a vector
 
 help(c) # What is the function c? the help function is your friend.
 
-y<- w*x # what happens when you multiply a scalar and vector?
+y <- w*x # what happens when you multiply a scalar and vector?
 
-z<-x*y # what happens when you multiply two vectors?
+z <- x*y # what happens when you multiply two vectors?
 
 z[2] # reference 2nd element in vector z
 
@@ -61,36 +61,36 @@ sd(z) # built-in function for taking standard deviation of vector
 
 sem(z) # what about standard error of the mean?
 
-sem<-function(x){
+sem <- function(x){
   sd(x)/sqrt(length(x))
   } # example of a simple user-defined function
 
   # Three common ways to create a matrix (two dimensional vectors)
 
   # First way: combine vectors
-j<- c(rnorm(length(z),mean=z)) # create new vector of random numbers
+j <- c(rnorm(length(z),mean=z)) # create new vector of random numbers
 
-k<-cbind(z,j) # use cbind to make a matrix from two vectors
+k <- cbind(z,j) # use cbind to make a matrix from two vectors
 
   # use 'help' function to learn about cbind
 
   # use the 'dim' function to describe the matrix you just created 
 
   # Second way: use matrix function
-l<-matrix(c(2,4,3,1,5,7),nrow=3,ncol=2) 
+l <- matrix(c(2,4,3,1,5,7),nrow=3,ncol=2) 
 
   # Third way: import data
-m<-as.matrix(read.table("matrix.txt",sep="\t",header=FALSE))
+m <- as.matrix(read.table("matrix.txt",sep="\t",header=FALSE))
 
-n<-t(m) # often we want to transpose matrices, this is easy in R
+n <- t(m) # often we want to transpose matrices, this is easy in R
 
   # Some ways to subset data in a matrix:
 
   # Take first three rows:
-n<-m[1:3,]
+n <- m[1:3,]
 
   # Take first two columns:
-n<-m[,1:2]
+n <- m[,1:2]
 
   # Take columns 1, 2, and 5:
-n<-m[,c(1:2,5)]
+n <- m[,c(1:2,5)]
