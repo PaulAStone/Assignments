332530390e91af342ed96a576c45c06c8195480f
Mario Muscarella <mmuscare@indiana.edu>
2015-02-25 09:19:34 -0500

    cleaning up MothurTools

--- PhyloCom/bin/MothurTools.R
+++ PhyloCom/bin/MothurTools.R
@@ -23,7 +23,9 @@
 #                                                                              #
 ################################################################################
 
-# Import OTU matrix
+require("reshape")||install.packages("reshape");require("reshape")
+
+# Import OTU Site-by-Species Matrix
 read.otu <- function(shared = " ", cutoff = "0.03"){
   matrix <- read.table(shared, header=T, fill=TRUE, comment.char="", sep="\t")
   matrix.cutoff <- subset(matrix, matrix$label == cutoff)
@@ -33,6 +35,7 @@ read.otu <- function(shared = " ", cutoff = "0.03"){
   return(matrix.out)
   }
 
+# Import Taxonomy Information
 read.tax <- function(taxonomy = " ", format = "rdp"){
   tax_raw <- read.delim(taxonomy)                 # load genus-level data
   if (format == "rdp"){
@@ -43,64 +46,3 @@ read.tax <- function(taxonomy = " ", format = "rdp"){
   }
   return(tax)
 }
-
-# Count All Groups
-count.groups <- function(otu.matrix = " "){
-  counts <- rowSums(otu.matrix)
-  return(counts)
-  }
-
-# rarefy function from Vegan (version 2.0-10)
-rrarefy.1 <- function (x, sample) {
-    if (!identical(all.equal(x, round(x)), TRUE))
-        stop("function is meaningful only for integers (counts)")
-    x <- as.matrix(x)
-    if (ncol(x) == 1)
-        x <- t(x)
-    if (length(sample) > 1 && length(sample) != nrow(x))
-        stop(gettextf("length of 'sample' and number of rows of 'x' do not match"))
-    sample <- rep(sample, length = nrow(x))
-    colnames(x) <- colnames(x, do.NULL = FALSE)
-    nm <- colnames(x)
-    for (i in 1:nrow(x)) {
-        row <- sample(rep(nm, times = x[i, ]), sample[i])
-        row <- table(row)
-        ind <- names(row)
-        x[i, ] <- 0
-        x[i, ind] <- row
-        }
-    return(x)
-    }
-
-# Subsampling wrapper
-sub.sample <- function(otu.matrix  = " ",
-                       sample.size = "min(count.groups(test))"){
-  counts <- count.groups(otu.matrix)
-  if (sample.size == " "){
-    sample.size = counts}
-  statement <- counts > sample.size  # Add warning message
-  otu.matrix <- subset(otu.matrix, rowSums(otu.matrix)>sample.size)
-  x <- rrarefy.1(otu.matrix, sample.size)
-  return(x)
-  }
-
-# Calculate Sample Depth or Coverage for Resampled Matrix
-coverage <- function(input= " ", cutoff = " ", size = " ", shared = "FALSE"){
-  if(shared == TRUE){
-    otu.matrix <- read.otu(input, cutoff)
-    } else {
-      otu.matrix <- input
-      }
-  counts <- count.groups(otu.matrix)
-  statement <- counts > size  # Add warning message
-  otu.matrix <- subset(otu.matrix, rowSums(otu.matrix)>size)
-  cover <- matrix(NA, dim(otu.matrix)[1], 1)
-  rownames(cover) <- rownames(otu.matrix)
-  colnames(cover) <- "Coverage"
-  temp.matrix <- sub.sample(otu.matrix, size)
-  for (i in 1:dim(temp.matrix)[1]){
-    cover[i,] <- 1 - ((length(which(temp.matrix[1,] == 1))) /
-        (length(which(temp.matrix[i,] > 0))))
-    }
-  return(cover)
-  }
